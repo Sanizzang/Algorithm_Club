6a699b28d89ed5b5813315ec46c5467b07d724bc
Sanizzang <tksl4732@naver.com>
2021-09-23 22:56:26 +0900

    Add Brute_Force directory

---
+++ Brute_Force/2309_Seven_dwarfs/Sanizzang.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int[] dwarfs = new int[9];
+		int sum = 0; 
+		for(int i = 0;i < 9;i++){
+			dwarfs[i] = sc.nextInt();
+			sum += dwarfs[i];
+		}
+		Arrays.sort(a);
+		for(int i = 0;i < 9;i++){
+			for(int j = i + 1;j < 9;j++){
+				if(sum - dwarfs[i] - dwarfs[j] == 100){
+					for(int k = 0;k < n;k++){
+						if(i == k || j == k){
+							continue;
+						}
+						System.out.println(dwarfs[k]);
+					}
+					System.exit(0);
+				}
+			}
+		}
+	}
+}
